4983e9b908328a333212986e531d4ec8ebb662ec
[Diana Wu] <diana.asap@gmail.com>
2019-02-13 21:24:28 -0600

    limit lazyload

--- src/components/items.vue
+++ src/components/items.vue
@@ -363,7 +363,8 @@ export default {
     },
     lazyLoad() {
       if (this.items.lazyLoading) return;
-      if (this.items.meta.total_count === this.items.data.length) return;
+      //if (this.items.meta.total_count === this.items.data.length) return;
+      if (this.items.page * this.formatParams().limit > this.items.data.length) return;
 
       this.items.lazyLoading = true;
       this.items.error = null;
